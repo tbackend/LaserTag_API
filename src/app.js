ecd1f5fbc15e6e86923fd1977d690719b24ee500
tbackend <tbackend642@gmail.com>
2025-03-25 12:40:55 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -4,5 +4,10 @@ import dataRouter from "../src/routes/data.routes.js";
 export const app = express();
 
 app.use(express.json());
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+    })
+);
 
 app.use("/api/v1/data", dataRouter);
--- src/routes/data.routes.js
+++ src/routes/data.routes.js
@@ -4,6 +4,5 @@ import { store } from "../controllers/data.controller.js";
 const router = Router();
 
 router.route("/").post(store);
-// router.route("/player").get(player);
 
 export default router;
